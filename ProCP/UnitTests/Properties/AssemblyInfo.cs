8e3af50edd9fa7ebb76b29a6e77fa9b2061b0b99
Aleksandar Georgiev <a.georgiev@student.fontys.nl>
2019-04-29 13:53:36 +0200

    unit tests project

--- ProCP/UnitTests/Properties/AssemblyInfo.cs
+++ ProCP/UnitTests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyTitle("UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyProduct("UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("9877b7a1-afce-4c3e-9a19-def61ad03f98")]
+[assembly: Guid("85417966-853d-4e39-9007-79cd39f6b450")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- ProCP/UnitTests/UnitTest1.cs
+++ ProCP/UnitTests/UnitTest1.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Tests
+namespace UnitTests
 {
     [TestClass]
     public class UnitTest1
